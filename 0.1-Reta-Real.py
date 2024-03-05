9390ca079f507c54279a7c29087d44c7d0212e13
HernaldoMeneses <140676491+HernaldoMeneses@users.noreply.github.com>
2024-03-05 08:17:58 -0300

    01.RetaReal.py

---
+++ 0.1-Reta-Real.py
@@ -0,0 +1,43 @@
+'''
+Números
+    Inteiros
+        ...,-3,-2,-1,0,1,2,3,...;
+        
+        seja: m,n inteiros
+            m + n => inteiro
+            m - n => inteiro
+            m * n => inteiro
+            m / n => inteiro ou racional (n != 0)
+
+            + Soma
+            - Operação inversa da Soma
+            * Multiplicação
+            / Operação inversa da Multiplicação
+
+            logo: m*n/n = m 
+                porém, se n = 0
+                    temos: m*0/0 = m (0/0 => indefinição)
+'''
+
+'''
+    Construção da Reta
+        Imagine uma reta horizontal, estendida indefinidamente tanto de um lado quanto de outro.
+            Escolha um ponto sobre essa reta e identifique-o como 0.
+            Escolha outro ponto à direita e identifique-o com 1.
+            Qualquer outra coisa será determinada pensando-se na distância entre 0 e 1 como unidade de comprimento.
+                2 situa-se uma unidade à direira de 1. Depois, 3 situa-se uma unidade a direita de 2 e assim por diante.
+                Inteiros negativos correspondem a mover-se para a esquerda de 0.
+
+            Seja n um inteiro positivo, então 1/n situa-se à direita de 0, distante poelo comprimento obtido pela divisão de segmento de 0 a 1 em n segmentos de igual comprimento.
+            Depois, 2/n situa-se à direita e à mesma distância de 1/n, e 3/n situa-se à direita e à mesma distância de 2/n novamente.
+
+            Os números racionais negativos estão situados, similarmente, sobre a reta, mas à esquerda de 0.
+
+            usaremos a noção intuitiva de que a reta não tem interrupções e de que toda distância admisível possa ser representada por um ponto sobre a reta.
+
+            è possível provar com ferramentas matematicas mais avançadas a continuidade da Reta Real, mas neste ponto do curso, utilizaremos apenas a noção intuitiva.
+'''
+
+#pag. 26/744
+#file:///L:/Linear_/Base-Jump_03/Sheldon%20Axler%20-%20Pr%C3%A9-C%C3%A1lculo.%20Uma%20Prepara%C3%A7%C3%A3o%20Para%20o%20C%C3%A1lculo-LTC%20(2016).pdf
+
