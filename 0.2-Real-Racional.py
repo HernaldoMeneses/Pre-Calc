ed2d64d8ba4588f1aeaea4feaef9c392d4a148e2
HernaldoMeneses <140676491+HernaldoMeneses@users.noreply.github.com>
2024-03-11 11:31:05 -0300

    Create 0.2-Real-Racional.py

---
+++ 0.2-Real-Racional.py
@@ -0,0 +1,39 @@
+'''
+    Todo Número Real é Racional?
+'''
+
+#Todo Número racional corresponde a um ponto sobre a reta real.
+#Será que todo ponto sobre a reta real corresponde a um número racional?
+#Os antigos gregos descobriram que isso não é veradeiro. como?
+#   Geometria
+##      Triângulo Retângulo
+###         Pitagoras: c^2=a^2+b^2
+####            há mais de 2500 anos.
+###         c^2 = 1^2+1^2
+###         :. c^2 = 2
+###         c existe? 1x1 = 1 e 2x2 = 4
+###
+###
+###         Desmontração por Contradição
+#
+#Suponha m e n com n != 0 e m e n sem fator comum |                             (m/n)^2 = 2
+#                                                                                   m^2 = 2*n^2
+#:.  m^2 é par => m é par, visto que o quadrado de impar é impar
+#:.  existe algum inteiro k | m=2k.
+# subistituindo
+#                                                                                 (2k)^2 = 2*n^2
+#                                                                                   4k^2 = 2*n^2
+#                                                                                   2k^2 = n^2
+#:. n é par, vide m^2 é par => m é par. (absurdo, visto que sem fator comun)
+# logo, não existe m en com n != 0 e m e n sem fator comum |                    (m/n)^2 = 2
+# logo,                                                    |                    (m/n)^2 != 2
+
+# "Quando se exlui o impossível, o que restar, ainda que imporvável, deve ser a verdade."
+# - Sherlock Holmes
+
+#Porém
+# 2^(1/2)^2 = 2
+#logo 2^(1/2) não é um número Racional
+#:. nem todo número real é um número racional. (irracional)
+
+#"irracional" "não baseado na razão"
